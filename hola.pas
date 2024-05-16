8ea3e78a0522ccbd1763b57cbef6c1ef48186e52
neriinazer <nerina.nazer@gmail.com>
2024-05-16 19:32:54 -0300

    Create hola.pas

---
+++ hola.pas
@@ -0,0 +1,13 @@
+program hola;
+var
+    num1, num2, suma : integer; 
+
+begin 
+    write ('ingrese el primer numero: '); 
+    read (num1); 
+    write ('ingrese el segundo numero: ') ;
+    read (num2); 
+    suma := num1 + num2; 
+    write ('la suma de ', num1, ' y ', num2, ' es ', suma);
+end.
+
